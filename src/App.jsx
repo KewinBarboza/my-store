c507766888e6cec2d8bf422b952b91c908dadd9e
kewinbarboza <kewinbarboza@gmail.com>
2021-08-15 20:59:52 -0400

    install fremework of form and ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,33 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import { List, ListItem } from '@chakra-ui/react'
+import { CheckCircleIcon } from '@chakra-ui/icons'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = async () => {
+    const res = await fetch('http://localhost:3000/productos')
+    const data = await res.json()
+    console.log(data)
+    setData(data)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <List spacing={3}>
+      {data && data.map(d => (
+          <ListItem spacing={3} key={d.id}>
+            <CheckCircleIcon mr={10} color="green.600" />
+            {d.nombre}
+          </ListItem>
+      ))}
+      </List>
+
     </div>
   )
 }
